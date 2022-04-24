2523f65682ca37fd510a82aa846c91b9538e2eac
Matt Bancroft <matthew.bancroft@styler.link>
2022-04-25 00:14:57 +0900

    Made model for url data and and called mock endpoint to render url list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+Future<List<UrlData>> fetchUrls() async {
+  final response =
+      await http.get(Uri.parse('https://apimocha.com/bookmarker/urls'));
+  if (response.statusCode == 200) {
+    List<dynamic> jsonResponse = jsonDecode(response.body)['items'];
+    return jsonResponse.map((data) => UrlData.fromJson(data)).toList();
+  } else {
+    throw Exception('Error fetching urls...');
+  }
+}
+
+class Tag {
+  final String id;
+  final String name;
+
+  Tag({required this.id, required this.name});
+}
+
+class UrlData {
+  final String id;
+  final bool isArchived;
+  final bool isRead;
+  final String? note;
+  final List tags;
+  final String title;
+  final String url;
+
+  UrlData(
+      {required this.id,
+      required this.isArchived,
+      required this.isRead,
+      this.note,
+      required this.tags,
+      required this.title,
+      required this.url});
+
+  factory UrlData.fromJson(Map<String, dynamic> json) {
+    return UrlData(
+      id: json['id'],
+      isArchived: json['is_archived'],
+      isRead: json['is_read'],
+      note: json['note'],
+      tags: json['tags'],
+      title: json['title'],
+      url: json['url'],
+    );
+  }
+}
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<List<UrlData>> futureUrls;
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  void initState() {
+    super.initState();
+    futureUrls = fetchUrls();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Number of times you pressed the button:',
+        title: 'Bookmarker',
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Bookmarker'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            body: Center(
+              child: FutureBuilder<List<UrlData>>(
+                future: futureUrls,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<UrlData> urls = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: urls.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        TextStyle _titleStyle;
+                        if (urls[index].isRead == false) {
+                          _titleStyle = _biggerFont.merge(
+                              const TextStyle(fontWeight: FontWeight.bold));
+                        } else {
+                          _titleStyle = _biggerFont;
+                        }
+                        return Container(
+                          height: 75,
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                    color: Theme.of(context).dividerColor)),
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                urls[index].url,
+                                style: _titleStyle,
+                              ),
+                              if (urls[index].note != null)
+                                Text(
+                                  urls[index].note!,
+                                  textAlign: TextAlign.left,
+                                ),
+                              //if (urls[index].tags.isNotEmpty)
+                              //for (dynamic tag in urls[index].tags)
+                              //Text(tag.name!.toString())
+                            ],
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return const CircularProgressIndicator();
+                },
+              ),
+            )));
   }
 }
